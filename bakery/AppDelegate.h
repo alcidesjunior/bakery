3401777b7b674719984e6b17f901d8ca6c76ae84
Ada 2018 <ada2018@172-16-2-164.0000.ifce.br.iosda.org>
2018-04-05 08:32:07 -0300

    Initial Commit

---
+++ bakery/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bakery
+//
+//  Created by Ada 2018 on 4/5/18.
+//  Copyright © 2018 Ada 2018. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ bakery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  bakery
+//
+//  Created by Ada 2018 on 4/5/18.
+//  Copyright © 2018 Ada 2018. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
